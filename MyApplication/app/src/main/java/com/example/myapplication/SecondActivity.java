8afc6457f1f6f603ddbf94fc15b7ed8d56c5a46d
MarDatsko <datskomar@gmail.com>
2019-08-03 17:30:13 +0300

    SecontActivity: create opportunity to save team names.
    Signed-off-by: Marian Datsko <datskomar@gmail.com>

--- MyApplication/app/src/main/java/com/example/myapplication/SecondActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
 
     private Button btn2;
+    private EditText team_1, team_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +28,18 @@ public class SecondActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View view) {
                         Intent int_btn2 = new Intent(SecondActivity.this, ThirdActivity.class);
+                        getTeamName();
                         startActivity(int_btn2);
                     }
                 }
         );
+    }
+
+    public void getTeamName(){
+        team_1 = (EditText)findViewById(R.id.editText3);
+        team_1.getText().toString();
+
+        team_2 = (EditText)findViewById(R.id.editText4);
+        team_2.getText().toString();
     }
 }
