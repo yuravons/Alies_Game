ba34ead774a59ecca847854c3c5c227fc1543551
yuravons <yura.vons@gmail.com>
2019-08-03 16:44:03 +0300

    Alies: Set text from seekBar to TextView.
    Signed-off-by: Yurii.Vons <yura.vons@gmail.com>

--- MyApplication/app/src/main/java/com/example/myapplication/ThirdActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ThirdActivity.java
@@ -6,15 +6,58 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class ThirdActivity extends AppCompatActivity {
 
     private Button btn3;
+    private SeekBar seekBar1, seekBar2;
+    private TextView textView1, textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
         addListenerOnButton2();
+
+        textView1 = (TextView) findViewById(R.id.textView7);
+        textView2 = (TextView) findViewById(R.id.textView8);
+
+        seekBar1 = (SeekBar) findViewById(R.id.seekBar1);
+        seekBar2 = (SeekBar) findViewById(R.id.seekBar2);
+
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textView1.setText("" + i + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textView2.setText("" + i + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     public void addListenerOnButton2() {
