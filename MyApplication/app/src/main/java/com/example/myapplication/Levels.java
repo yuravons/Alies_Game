18ad5cae53faae02bd9efbea76f4a302bb12cbc6
MarDatsko <datskomar@gmail.com>
2019-08-03 18:42:23 +0300

    Levels: Create opportunity to move to a new layer after choose levels.
    Signed-off-by: Marian Datsko <datskomar@gmail.com>

--- MyApplication/app/src/main/java/com/example/myapplication/Levels.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Levels.java
@@ -2,13 +2,57 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Levels extends AppCompatActivity {
+
+    private Button btn0,btn01,btn02;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_levels);
+        addListenerOnButton();
+    }
+    
+    public void addListenerOnButton() {
+        btn0 = (Button) findViewById(R.id.button5);
+        btn01 = (Button) findViewById(R.id.button6);
+        btn02 = (Button) findViewById(R.id.button7);
+
+        btn0.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent int_btn1 = new Intent(Levels.this, StartGame.class);
+                        startActivity(int_btn1);
+                    }
+                }
+
+        );
+
+        btn01.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent int_btn1 = new Intent(Levels.this, StartGame.class);
+                        startActivity(int_btn1);
+                    }
+                }
+
+        );
+
+        btn02.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent int_btn1 = new Intent(Levels.this, StartGame.class);
+                        startActivity(int_btn1);
+                    }
+                }
+        );
     }
 }
