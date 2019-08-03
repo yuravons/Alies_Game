911477de8318c857295681d0fac6844f28c99eb6
MarDatsko <datskomar@gmail.com>
2019-08-03 16:00:22 +0300

    Alies: Add third activity window to set game option.
    Signed-off-by: Marian Datsko <datskomar@gmail.com>

--- MyApplication/app/src/main/java/com/example/myapplication/SecondActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -2,13 +2,33 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        addListenerOnButton1();
+    }
+
+
+    public void addListenerOnButton1() {
+        btn2 = (Button) findViewById(R.id.button3);
+        btn2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent int_btn2 = new Intent(SecondActivity.this, ThirdActivity.class);
+                        startActivity(int_btn2);
+                    }
+                }
+        );
     }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/ThirdActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ThirdActivity.java
@@ -2,13 +2,31 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends AppCompatActivity {
 
+    private Button btn3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        addListenerOnButton2();
+    }
+
+    public void addListenerOnButton2() {
+        btn3 = (Button) findViewById(R.id.button4);
+        btn3.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent int_btn2 = new Intent(ThirdActivity.this, FourthActivity.class);
+                        startActivity(int_btn2);
+                    }
+                }
+        );
     }
 }
