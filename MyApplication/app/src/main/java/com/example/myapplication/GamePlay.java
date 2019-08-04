8f13fec73e17deb6da316b1614f39000b3654656
yuravons <yura.vons@gmail.com>
2019-08-04 14:41:42 +0300

    GamePlay: Add timer to GamePlay.
    Signed-off-by: Yura Vons <yura.vons@gmail.com>

--- MyApplication/app/src/main/java/com/example/myapplication/GamePlay.java
+++ MyApplication/app/src/main/java/com/example/myapplication/GamePlay.java
@@ -2,23 +2,88 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.sql.Time;
+import java.util.Locale;
+import java.util.Timer;
 
 public class GamePlay extends AppCompatActivity {
 
     private String sTeam1_Name, sTeam2_Name;
     private String sTime;
     private TextView tvTime;
+    private Button btnStart;
+    private CountDownTimer mCountDownTimer;
+    private boolean mTimerRunning;
 
+    private long mTimeLeftMilliSeconds;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_play);
 
         sTime = getIntent().getStringExtra("TIME");
+        mTimeLeftMilliSeconds = Integer.parseInt(sTime) * 1000;
         tvTime  = (TextView)findViewById(R.id.textView21);
-        tvTime.setText(sTime);
+        btnStart = (Button) findViewById(R.id.button11);
 
+        addListenerOnButton();
+        updateCountDownText();
+    }
+
+    public void addListenerOnButton() {
+        btnStart.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (mTimerRunning) {
+                            pauseTimer();
+                        } else {
+                            startTimer();
+                        }
+                    }
+                }
+        );
+    }
+
+    private void startTimer() {
+        mCountDownTimer = new CountDownTimer(mTimeLeftMilliSeconds, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftMilliSeconds = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                btnStart.setText("Start");
+                btnStart.setVisibility(View.INVISIBLE);
+            }
+        }.start();
+
+        mTimerRunning = true;
+        btnStart.setText("pause");
+    }
+
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        btnStart.setText("Start");
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) (mTimeLeftMilliSeconds / 1000) / 60;
+        int seconds = (int) (mTimeLeftMilliSeconds / 1000) % 60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+        tvTime.setText(timeLeftFormatted);
     }
 }
