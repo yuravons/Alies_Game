40376af67e95fab97c3f0c59b4f9441464aad463
yuravons <yura.vons@gmail.com>
2019-08-04 13:57:24 +0300

    GamePlay: Receive time on GamePlay.
    Signed-off-by: Yura Vons <yura.vons@gmail.com>

--- MyApplication/app/src/main/java/com/example/myapplication/GamePlay.java
+++ MyApplication/app/src/main/java/com/example/myapplication/GamePlay.java
@@ -3,12 +3,22 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GamePlay extends AppCompatActivity {
+
+    private String sTeam1_Name, sTeam2_Name;
+    private String sTime;
+    private TextView tvTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_play);
+
+        sTime = getIntent().getStringExtra("TIME");
+        tvTime  = (TextView)findViewById(R.id.textView21);
+        tvTime.setText(sTime);
+
     }
 }
